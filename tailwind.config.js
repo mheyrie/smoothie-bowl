99ffa49696a3278434a3110d1220a5ee0ff22768
Mheyrie <omota4fidelity@gmail.com>
2024-11-14 20:32:15 +0100

    Image updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        rubik: ['Rubik', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
-        arima: ['Arima', 'system-ui'],
+        rubik: ["Rubik", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        arima: ["Arima", "system-ui"],
       },
       colors: {
-        BrandCerulean: '#2D517B',
-        BrandWhite: '#FFFFFF',
-        BrandOffWhite: '#E6E7E8',
-        BrandCyan: '#7DCCC0',
-        BrandCyanPurge: '#49C7EC',
-        BrandPale: '#E8F6FF',
+        PrimaryY: "#FFF7CF",
+        SecondaryY: "#F5DE6E",
+        PrimaryP: "#FFE1E2",
+        SecondaryP: "#FCB1B4",
+        PrimaryG: "#ECFFCC",
+        SecondaryG: "#8FB351",
+        PrimaryDp: "#FFCBEA",
+        SecondaryDp: "#C976A7",
+        HoverOne: "#D1BC5A",
+        HoverTwo: "#C67679",
+        HoverThr: "#5E8220",
+        HoverFor: "#F1AEDC",
       },
-     
     },
   },
   plugins: [],
