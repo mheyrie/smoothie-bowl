1218938c35e70a2704403a24df1f2472733612f6
Mheyrie <omota4fidelity@gmail.com>
2024-11-14 20:03:32 +0100

    Cleaning up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        arima: ['Arima', 'system-ui'],
+      },
+      colors: {
+        BrandCerulean: '#2D517B',
+        BrandWhite: '#FFFFFF',
+        BrandOffWhite: '#E6E7E8',
+        BrandCyan: '#7DCCC0',
+        BrandCyanPurge: '#49C7EC',
+        BrandPale: '#E8F6FF',
+      },
+     
+    },
   },
   plugins: [],
-}
+};
