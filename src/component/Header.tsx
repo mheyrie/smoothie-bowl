224b3a5da0b7396e911fec1c30de84e9302526fd
Mheyrie <omota4fidelity@gmail.com>
2024-11-16 00:16:18 +0100

    Working on header

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,5 +1,16 @@
 export default function Header() {
   return (
-    <div className="text-red-700">Header</div>
+    <nav className="text-red-700">
+        <div className="">Logo</div>
+        <div className="">
+            <ul className="">
+                <li className="">All Recipes</li>
+                <li className="">Vegan</li>
+                <li className=""></li>
+                <li className=""></li>
+                <li className=""></li>
+            </ul>
+        </div>
+    </nav>
   )
 }
