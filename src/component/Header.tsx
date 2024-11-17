d950baa1abfbd2c90e78d67516b8357598cfdb05
Mheyrie <omota4fidelity@gmail.com>
2024-11-17 14:04:41 +0100

    updated

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,19 +1,17 @@
-
-
 export default function Header() {
   return (
-    <nav className="flex justify-between">
-        <div className="">
-            <img src='/logo512.png' alt="header-logo" />
-        </div>
-        <div className="">
-            <ul className="flex justify-around">
-                <li className="">All Recipes</li>
-                <li className="">Vegan</li>
-                <li className="">Gluten-Free</li>
-                <li className="">Shop</li>
-                           </ul>
-        </div>
+    <nav className="flex justify-between w-full p-8">
+      <div className="">
+        <img src="/images/logo/logo.png" alt="header-logo" className="w-10 h-10" />
+      </div>
+      <div className="">
+        <ul className="flex justify-between gap-6">
+          <li className="">All Recipes</li>
+          <li className="">Vegan</li>
+          <li className="">Gluten-Free</li>
+          <li className="">Shop</li>
+        </ul>
+      </div>
     </nav>
-  )
+  );
 }
