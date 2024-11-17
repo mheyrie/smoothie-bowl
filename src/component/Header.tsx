ee8451fc26466c4dc51b73b044d61820b731edd9
Mheyrie <omota4fidelity@gmail.com>
2024-11-17 14:31:23 +0100

    Updated

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -10,10 +10,10 @@ export default function Header() {
       </div>
       <div className="">
         <ul className="flex justify-between gap-8">
-          <li className="cursor-pointer">All Recipes</li>
-          <li className="cursor-pointer">Vegan</li>
-          <li className="cursor-pointer">Gluten-Free</li>
-          <li className="cursor-pointer">Shop</li>
+          <li className="cursor-pointer font-semibold">All Recipes</li>
+          <li className="cursor-pointer font-semibold">Vegan</li>
+          <li className="cursor-pointer font-semibold">Gluten-Free</li>
+          <li className="cursor-pointer font-semibold">Shop</li>
         </ul>
       </div>
     </nav>
